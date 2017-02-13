162942af8dc2ee160fd16801f9a8d2f92a7ebd40
Mohammed Naji <moha.buy@gmail.com>
2017-02-13 14:31:47 +0200

    files

---
+++ src.js
@@ -0,0 +1,37 @@
+//The Location Funtion
+function request(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+         // console.log(json)
+            cb(undefined, json)
+
+        } else {
+            console.log("waiting for response");
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+function getlocation(callback) {
+    request("https://wtfismyip.com/json", callback)
+}
+
+getlocation(function(error, data) {
+    // console.log(data)
+    var places="Jordan";
+    sts(places,function(error,data){
+    console.log(data[0])
+    
+  })
+      return data
+});
+
+function sts(place, callback) {
+// console.log(c)
+    request("http://api.population.io:80/1.0/population/1980/" + place + "/", callback)
+}
+
+
