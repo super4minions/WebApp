c4b4b43efa56138ceb9e23cee3e25a02f0e672cc
Mohammed Naji <moha.buy@gmail.com>
2017-02-14 14:49:46 +0200

    somechanges

--- src.js
+++ src.js
@@ -1,17 +1,19 @@
 //The Location Funtion
 function request(url, cb) {
     var xhr = new XMLHttpRequest();
+
     xhr.onreadystatechange = function() {
+
         if (xhr.readyState === 4 && xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
-         // console.log(json)
+ console.log("THis is console log",json)
             cb(undefined, json)
-
         } else {
             console.log("waiting for response");
         }
     };
     xhr.open("GET", url, true);
+      // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhr.send();
 }
 
@@ -19,28 +21,36 @@ function getlocation(callback) {
     request("https://wtfismyip.com/json", callback)
 }
 
-getlocation(function(error, data) {
-	var p=data.YourFuckingLocation.split(",");
+// getlocation(function(error, data) {
+//     var p = data.YourFuckingLocation.split(",");
+//     var places = p[2]
+//     places = places.trim();
+//     if (places == "Occupied Palestine") {
+//         places = "West Bank and Gaza"
+//     }
 
-var places= p[2]
-   places=places.trim();
-if (places=="Occupied Palestine"){
+//     sts(places, function(error, data) {
+//         console.log(data)
 
-	places="West Bank and Gaza"
-}
-  
-    //var fullC=data.
- 
-    sts(places,function(error,data){
-    console.log(data[0])
     
-  })
-      return data
-});
+//     })
+// });
 
 function sts(place, callback) {
-// console.log(c)
-    request("http://api.population.io:80/1.0/population/1980/" + place + "/", callback)
+    // console.log(c)
+    request("http://api.population.io/1.0/population/2016/France/", callback)
 }
 
-س
+function contries(callback) {
+    // console.log(c)
+    request("http://api.population.io:80/1.0/countries", callback)
+}
+
+// contries(function(error, data) {
+//     console.log(data)
+// });
+
+function chart(data,callback){
+	   // request("http://api.population.io:80/1.0/population/1980/" + place + "/", callback)
+
+}
