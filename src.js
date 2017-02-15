4b25d55b4c7ca47d0e3c91416175442d29798e95
Mohammed Naji <moha.buy@gmail.com>
2017-02-15 10:43:07 +0200

    i don't know

--- src.js
+++ src.js
@@ -22,7 +22,7 @@ function getlocation(callback) {
 }
 
 // getlocation(function(error, data) {
-<<<<<<< HEAD
+
 //     var p = data.YourFuckingLocation.split(",");
 //     var places = p[2]
 //     places = places.trim();
@@ -31,36 +31,18 @@ function getlocation(callback) {
 //     }
 
 //     sts(places, function(error, data) {
-//         console.log(data)
+//        // console.log(data)
 
     
 //     })
-=======
-// 	console.log(data);
-//     var p=data.YourFuckingLocation.split(",");
-//     var places= p[2]
-//        places=places.trim();
-//     if (places=="Occupied Palestine"){
-
-//     	places="West Bank and Gaza"
-//     }
-  
-//     //var fullC=data.
- 
-//     sts(places,function(error,data){
-//     console.log(data[0])
-    
-//   })
 //       return data
->>>>>>> 9709f9e4499f1a2050c791db6fd364f004da5892
 // });
 
 function sts(place, callback) {
-    // console.log(c)
+
     request("http://api.population.io/1.0/population/2016/France/", callback)
 }
 
-<<<<<<< HEAD
 function contries(callback) {
     // console.log(c)
     request("http://api.population.io:80/1.0/countries", callback)
@@ -69,10 +51,3 @@ function contries(callback) {
 // contries(function(error, data) {
 //     console.log(data)
 // });
-
-function chart(data,callback){
-	   // request("http://api.population.io:80/1.0/population/1980/" + place + "/", callback)
-
-}
-=======
->>>>>>> 9709f9e4499f1a2050c791db6fd364f004da5892
